bec2551b7ec6bb7eebc7198ad2022c251707b4cf
Danilo Viacava <daniloviacava.acidlab@latam.com>
2023-08-22 08:43:03 -0500

    Updates

---
+++ precache-manifest.18284bf09f58fb90f1f621ae91c0353c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f493002809ec1c6404f70cc1bc50689f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbea7267b745b3b2acb4",
+    "url": "/static/css/main.7ab05e67.chunk.css"
+  },
+  {
+    "revision": "5934dafd25e3e010c670",
+    "url": "/static/js/2.ae07c98a.chunk.js"
+  },
+  {
+    "revision": "dbea7267b745b3b2acb4",
+    "url": "/static/js/main.8cb52fa6.chunk.js"
+  },
+  {
+    "revision": "e5328eea4f6de52b1155",
+    "url": "/static/js/runtime-main.06fd8d53.js"
+  }
+]);
